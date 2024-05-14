2670146b51a52f46965cc35a616a445287a56825
ketan lab <ketanbhogshetti@gmail.com>
2024-05-14 20:46:39 +0530

    guide page ready

---
+++ src/navigation/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./home.css"
+const Home = () => {
+    return (
+        <>
+        <div className='Home-Page-Container'>
+            <div className='upper-msg'>
+                <div className="upper-welcome-section">
+                    <h1>Welcome to Notes Master</h1>
+                    <p>Your Note-taking and Task Management Solution</p>
+
+                </div>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Home;
